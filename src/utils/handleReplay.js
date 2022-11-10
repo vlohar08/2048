eac8ad77fade687e599880e92997048a8ad1c051
Vishal Lohar <www.vlohar08@gmail.com>
2022-11-10 07:17:57 +0530

    Removed Replay Start Delay

--- src/utils/handleReplay.js
+++ src/utils/handleReplay.js
@@ -2,17 +2,21 @@ const handleReplay = (gameData, updateGame) => {
   if (gameData.isReplaying) return;
   let i = 0;
   let totalReplays = gameData.replay.length - 1;
-  let replay = setInterval(() => {
-    if (i === totalReplays) clearInterval(replay);
-    updateGame((prevGameData) => {
-      return {
-        ...prevGameData,
-        isReplaying: i === totalReplays ? false : true,
-        board: gameData.replay[i],
-      };
-    });
-    setTimeout(() => i++, 100);
-  }, 1000);
+  let replay = setInterval(
+    (function replayGame() {
+      if (i === totalReplays) clearInterval(replay);
+      updateGame((prevGameData) => {
+        return {
+          ...prevGameData,
+          isReplaying: i === totalReplays ? false : true,
+          board: gameData.replay[i],
+        };
+      });
+      setTimeout(() => i++, 100);
+      return replayGame;
+    })(),
+    2000
+  );
 };
 
 export default handleReplay;
